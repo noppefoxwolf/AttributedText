d477d058dbaa9b73e1a2973f9758f4c184a49942
noppe <noppelabs@gmail.com>
2026-01-01 20:14:50 +0900

    Update AttributedText.swift

--- Sources/AttributedText/AttributedText.swift
+++ Sources/AttributedText/AttributedText.swift
@@ -24,6 +24,7 @@ public struct AttributedText: UIViewRepresentable {
         textView.delegate = context.coordinator
         textView.allowsEditingTextAttributes = true
         textView.isEditable = false
+        textView.dataDetectorTypes = []
         
         // Layout
         textView.textContainerInset = .zero
