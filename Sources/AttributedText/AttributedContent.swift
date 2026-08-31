c62d2c8b3a5b98d5c1fdb4ede3fc6d2b45b91a41
noppe <noppelabs@gmail.com>
2026-08-31 21:04:06 +0900

    remove unused code

--- Sources/AttributedText/AttributedContent.swift
+++ Sources/AttributedText/AttributedContent.swift
@@ -9,23 +9,3 @@ struct RenderedContentConfiguration: Equatable {
     let textAlignment: NSTextAlignment
     let lineSpacing: CGFloat
 }
-
-// 参照ポインタの違いなどで見た目が同じであるのにequalにならない事があるので自前で比較する
-final class AttributedTextContent: Equatable {
-    let content: NSAttributedString
-
-    init(_ content: NSAttributedString) {
-        self.content = content
-    }
-
-    static func == (lhs: AttributedTextContent, rhs: AttributedTextContent) -> Bool {
-        lhs.content.isEqual(to: rhs.content)
-    }
-}
-
-extension UITextView {
-    var attributedTextContent: AttributedTextContent {
-        get { AttributedTextContent(attributedText) }
-        set { attributedText = newValue.content }
-    }
-}
--- Tests/AttributedTextTests/AttributedTextContentTests.swift
+++ Tests/AttributedTextTests/AttributedTextContentTests.swift
@@ -6,41 +6,6 @@ import UIKit
 @Suite
 struct AttributedTextContentTests {
     @Test
-    func identicalAttributesAreEqual() {
-        var lhs = AttributedString("Same text")
-        lhs.font = UIFont.systemFont(ofSize: 17)
-        lhs.foregroundColor = .label
-
-        var rhs = AttributedString("Same text")
-        rhs.font = UIFont.systemFont(ofSize: 17)
-        rhs.foregroundColor = .label
-
-        #expect(AttributedTextContent(NSAttributedString(lhs)) == AttributedTextContent(NSAttributedString(rhs)))
-    }
-
-    @Test
-    func differentFontsAreNotEqualWhenTheStringIsTheSame() {
-        var regular = AttributedString("Same text")
-        regular.font = UIFont.systemFont(ofSize: 17)
-
-        var large = AttributedString("Same text")
-        large.font = UIFont.systemFont(ofSize: 24)
-
-        #expect(AttributedTextContent(NSAttributedString(regular)) != AttributedTextContent(NSAttributedString(large)))
-    }
-
-    @Test
-    func differentColorsAreNotEqualWhenTheStringIsTheSame() {
-        var red = AttributedString("Same text")
-        red.foregroundColor = .systemRed
-
-        var blue = AttributedString("Same text")
-        blue.foregroundColor = .systemBlue
-
-        #expect(AttributedTextContent(NSAttributedString(red)) != AttributedTextContent(NSAttributedString(blue)))
-    }
-
-    @Test
     func defaultLineBreakStrategyMatchesSwiftUIText() {
         let content = NSAttributedString("Text")
 
